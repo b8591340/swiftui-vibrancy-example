d448cee66c68349144e92c31fa417db6ea301505
b8591340 <70052269+b8591340@users.noreply.github.com>
2026-05-11 14:07:20 +0200

    Local changes for swiftui-vibrancy-example

--- Vibrancy/ContentView.swift
+++ Vibrancy/ContentView.swift
@@ -5,33 +5,30 @@
 //  Created by Philip Davis on 1/14/21.
 //
 
-import SwiftUIVisualEffects
 import SwiftUI
+import SwiftUIVisualEffects
 
 struct ContentView: View {
-    private var vibrancyLabels: [UIVibrancyEffectStyle] = [ .label, .secondaryLabel, .tertiaryLabel, .quaternaryLabel]
-    private var vibrancyLabelsNames: [String] = [ "Label", "Secondary Label", "Tertiary Label", "Quaternary Label"]
-    
-    private var vibrancyFills: [UIVibrancyEffectStyle] = [ .fill, .secondaryFill, .tertiaryFill]
-    private var vibrancyFillNames: [String] = [ "Fill", "Secondary", "Tertiary"]
-    
+    private var vibrancyLabels: [UIVibrancyEffectStyle] = [.label, .secondaryLabel, .tertiaryLabel, .quaternaryLabel]
+    private var vibrancyLabelsNames: [String] = ["Label", "Secondary Label", "Tertiary Label", "Quaternary Label"]
+
+    private var vibrancyFills: [UIVibrancyEffectStyle] = [.fill, .secondaryFill, .tertiaryFill]
+    private var vibrancyFillNames: [String] = ["Fill", "Secondary", "Tertiary"]
+
     var body: some View {
         ZStack {
             Image("background")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            
-            
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/ .all/*@END_MENU_TOKEN@*/)
+
             VStack {
-                
                 Spacer()
-                
+
                 ZStack {
                     Color(.clear).blurEffect()
                         .blurEffectStyle(.systemThinMaterial)
-                    
+
                     TabView {
                         HStack(spacing: 30) {
                             ForEach(0..<vibrancyFills.count) { i in
@@ -40,7 +37,7 @@ struct ContentView: View {
                                         .font(.system(size: 60))
                                         .vibrancyEffect()
                                         .vibrancyEffectStyle(vibrancyFills[i])
-                                    
+
                                     Text(vibrancyFillNames[i])
                                         .fontWeight(.bold)
                                         .vibrancyEffect()
@@ -48,31 +45,21 @@ struct ContentView: View {
                                 }
                             }
                         }
-                        
+
                         VStack(alignment: .leading) {
                             ForEach(0..<vibrancyLabels.count) { i in
                                 Text(vibrancyLabelsNames[i])
-                                    .font(Font/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
+                                    .font(Font.title.bold())
                                     .vibrancyEffect()
                                     .vibrancyEffectStyle(vibrancyLabels[i])
                             }
                         }
-                        
-                        
-                        
-                        
                     }
-                    
+
                     .tabViewStyle(PageTabViewStyle())
                 }.frame(height: 400)
-                
-                
-                
-                
-            }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            
-            
+
+            }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/ .all/*@END_MENU_TOKEN@*/)
         }
     }
 }
@@ -80,6 +67,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            
     }
 }
